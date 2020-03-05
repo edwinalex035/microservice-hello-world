f1c1d3adf224d482568b6e289253a18308e158e3
Edwin Sanchez <edwinalex035@gmail.com>
2020-03-05 16:45:12 -0600

    :loud_sound: Adding log to HelloWorldController
    Adding log to HelloWorldController

--- helloWorld/src/main/java/com/ed/helloworld/controllers/HelloWorldController.java
+++ helloWorld/src/main/java/com/ed/helloworld/controllers/HelloWorldController.java
@@ -1,16 +1,23 @@
 package com.ed.helloworld.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+  @Value("${server.port}")
+  private String serverPort;
+
   @GetMapping
   public String helloWorld() {
+    log.info("Generating a random multiplication from server @ {}", serverPort);
     return "Hello World";
   }
-
 }
